76b3dce92c77d0f3afd1d8487b15056ac31629ab
ReboreExplore <manpabarmanaec@gmail.com>
2021-11-02 09:39:22 +0530

    add flower_of_life.py

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 This repository containes some math patterns:
 
-1. Flower of Life Exercise -
+1. Flower of Life Exercise - This is two folder flower of design symmmetry.
+
+To run the code
+
+```
+$ pip install -r requirements.txt
+$ python lower_of_life.py
+```
--- codes/flower_of_life.py
+++ codes/flower_of_life.py
@@ -1,1 +1,55 @@
-from turtle import *
+# All necessery imports
+import turtle
+import time
+WIDTH, HEIGHT = 500, 500 # specifies the height and width of the screen
+RADIUS = 100
+
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(HEIGHT,WIDTH)
+    screen.title('Flower of Life')
+    screen.bgcolor("black")
+
+def create_turtle():
+    t = turtle.Turtle()
+    t.speed(9)
+    t.color('red')
+    t.shape('turtle')
+    return t
+
+def create_first_seed(t):
+    t.circle(RADIUS)
+    t.left(90)
+    t.penup()
+    t.forward(RADIUS)
+    t.pendown()
+    for i in range(6):
+        t.right(60)
+        t.circle(RADIUS)
+
+def create_one_folds(colors,t,fold,r):
+    t.color(colors)
+    t.penup()
+    t.right(30)
+    t.forward(RADIUS)
+    t.right(90)
+    t.pendown()
+    t.right(60)
+    t.circle(RADIUS)
+    t.left(60)
+    for i in range(fold):
+        if i%2==0 and i!=0:
+            t.penup()
+            t.left(90)
+            t.forward(RADIUS)
+            t.right(150)
+            t.pendown()
+        t.circle(RADIUS)
+        t.left(60)
+    
+init_turtle()
+t = create_turtle()
+create_first_seed(t)
+create_one_folds('blue',t,12,2)
+
+time.sleep(5)
